fb7c1e2e7e91f74c7978cfe7ccee91123882c538
Brainless-Loco <dev.tcdas@gmail.com>
2024-02-24 23:13:57 +0600

    Log In functionality Added

---
+++ App.js
@@ -0,0 +1,31 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import LogIn from './screens/LogIn/LogIn';
+
+export default function App() {
+
+  const Stack = createNativeStackNavigator()
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+              name="LogInScreen"
+              component={LogIn}
+              options={{headerShown:false}}
+            />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
