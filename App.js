2320db3edc8504f129c72fb00f87b6990058abc4
Brainless-Loco <dev.tcdas@gmail.com>
2024-03-01 01:09:04 +0600

    Bottom Tabs Implemented for Dashboard

--- App.js
+++ App.js
@@ -1,8 +1,7 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 import LogIn from './screens/LogIn/LogIn';
+import Dashboard from './screens/Dashboard/Dashboard';
 
 export default function App() {
 
@@ -11,21 +10,10 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen
-              name="LogInScreen"
-              component={LogIn}
-              options={{headerShown:false}}
-            />
+        <Stack.Screen name="Dashboard" component={Dashboard} options={{headerShown:false}} />
+        <Stack.Screen name="LogInScreen" component={LogIn} options={{headerShown:false}} />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ screens/Dashboard/Dashboard.js
@@ -0,0 +1,69 @@
+import { View, Text } from 'react-native'
+import React, { useState } from 'react'
+import { StyleSheet } from 'react-native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Others from '../../components/Dashboard/Tabs/Others/Others'
+import Notification from '../../components/Dashboard/Tabs/Notification/Notification'
+import Profile from '../../components/Dashboard/Tabs/Profile/Profile'
+import Home from '../../components/Dashboard/Tabs/Home/Home'
+import { AntDesign } from '@expo/vector-icons';
+import { Ionicons } from '@expo/vector-icons';
+import { FontAwesome } from '@expo/vector-icons';
+
+const Tab = createBottomTabNavigator();
+
+export default function Dashboard() {
+
+  const focusedIcons = [
+    <Ionicons name="home-outline" size={24} color="black" />,
+    <Ionicons name="notifications-outline" size={24} color="black" />,
+    <FontAwesome name="user-o" size={24} color="black" />,
+    <AntDesign name="infocirlceo" size={24} color="black" />
+  ]
+
+  const unFocusedIcons = [
+    <Ionicons name="home-sharp" size={24} color="#0044ba" />,
+    <Ionicons name="notifications-sharp" size={24} color="#0044ba" />,
+    <FontAwesome name="user" size={24} color="#0044ba" />,
+    <AntDesign name="infocirlce" size={24} color="#0044ba" /> 
+  ]
+  
+  const tabComponents = [
+    {
+      name:"Home",component:Home, 
+      focusedIcon: focusedIcons[0],unFocusedIcon:unFocusedIcons[0]
+    },
+    {
+      name:"Notifications",component:Notification, 
+      focusedIcon: focusedIcons[1],unFocusedIcon:unFocusedIcons[1]
+    }, 
+    {
+      name:"Profile",component: Profile, focusedIcon:focusedIcons[2],unFocusedIcon:unFocusedIcons[2]
+    }, 
+    {
+      name:"Others",component:Others, focusedIcon:focusedIcons[3],unFocusedIcon:unFocusedIcons[3]
+    }
+  ]
+
+  return (
+    <Tab.Navigator >
+      {
+        tabComponents.map(({name,component,focusedIcon,unFocusedIcon})=>{
+          return(
+            <Tab.Screen name={name} component={component} options={{headerShown:false,tabBarIcon:({focused})=>(focused?unFocusedIcon:focusedIcon)}}/>
+          )
+        })
+      }
+    </Tab.Navigator>
+  )
+}
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
