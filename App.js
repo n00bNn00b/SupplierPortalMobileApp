15c650c383a6b65408b919d07535c1938264a431
n00bNn00b <dewanreal@gmail.com>
2024-03-20 22:34:50 +0600

    test commit

---
+++ App.js
@@ -0,0 +1,19 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import LogIn from './screens/LogIn/LogIn';
+import Dashboard from './screens/Dashboard/Dashboard';
+
+export default function App() {
+
+  const Stack = createNativeStackNavigator()
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="LogInScreen" component={LogIn} options={{headerShown:false}} />
+        <Stack.Screen name="Dashboard" component={Dashboard} options={{headerShown:false}} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
---
+++ local/Supabase.js
@@ -0,0 +1,17 @@
+import { AppState } from 'react-native'
+import 'react-native-url-polyfill/auto'
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = process.env.SUPABASEURL
+const supabaseAnonKey = process.env.SUPABASEANONKEY
+
+console.log(supabaseUrl)
+export const supabaseCreateClient = createClient(supabaseUrl, supabaseAnonKey, {
+    auth: {
+        storage: AsyncStorage,
+        autoRefreshToken: true,
+        persistSession: true,
+        detectSessionInUrl: false,
+    },
+})
